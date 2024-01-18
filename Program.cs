1e8aeb24c698e2c68771155d0f9348ee4eac3c44
amorimfe7 <amorimfe7@gmail.com>
2024-01-18 11:00:00 -0300

    estrutura do Cronômetro

--- Program.cs
+++ Program.cs
@@ -3,11 +3,19 @@
     class Program{
 
         static void Main (string[] args){
-            
+
             Console.WriteLine("Hello, World!");
 
         }
 
+        static void Start(){
+            int time = 10;
+            int currentTime = 0;
+
+            while(currentTime != time){
+                currentTime++;
+            }
+        }
     }
 }
 
